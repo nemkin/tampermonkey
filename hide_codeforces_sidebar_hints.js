08a482cd2674f6570b0599a1cbba9e543df1040e
Viktória Nemkin <viktoria.nemkin@gmail.com>
2024-08-08 10:02:53 +0200

    Create hide_codeforces_sidebar_hints.js

---
+++ hide_codeforces_sidebar_hints.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Hide Codeforces hints
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Hide the sidebar on Codeforces task descriptions.
+// @author       You
+// @match        https://codeforces.com/contest/*/problem/*
+// @match        https://codeforces.com/problemset/problem/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=codeforces.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     const sidebar = document.getElementById("sidebar");
+     sidebar.remove();
+     const content = document.getElementById("pageContent");
+     content.classList.remove("content-with-sidebar");
+})();
